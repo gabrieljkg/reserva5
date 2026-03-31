edcc600ddb944d4c7395bdaae988a820adbd43a8
Gabriel Calid Brito de Almeida <gabrielcalid@gmail.com>
2026-03-31 17:19:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,24 @@ import { ListProperty } from './pages/ListProperty';
 import { Profile } from './pages/Profile';
 import { AdminReservations } from './pages/AdminReservations';
 import { Success } from './pages/Success';
+import { WhatsAppButton } from './components/WhatsAppButton';
 
-import { CartProvider } from './context/CartContext';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import WhatsAppButton from './components/WhatsAppButton';
+import { CartProvider, useCart } from './context/CartContext';
 
 export default function App() {
   return (
     <CartProvider>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/imovel/:id/:slug" element={<ListingDetail />} />
+          <Route path="/imovel/:id/:slug?" element={<ListingDetail />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/anunciar" element={<ListProperty />} />
           <Route path="/perfil" element={<Profile />} />
           <Route path="/admin/reservas" element={<AdminReservations />} />
           <Route path="/sucesso" element={<Success />} />
         </Routes>
-        <Footer />
-        
-        {/* O BOTÃO APARECERÁ AQUI EM TODAS AS PÁGINAS */}
         <WhatsAppButton />
-        
       </BrowserRouter>
     </CartProvider>
   );
